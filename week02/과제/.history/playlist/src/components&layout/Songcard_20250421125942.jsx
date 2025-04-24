c467c42fe053ca84fd0436bc136aad9d9930bac9
SeungJu <lsjxcs@gmail.com>
2025-04-24 16:28:04 +0900

    rm

---
+++ week02/과제/.history/playlist/src/components&layout/Songcard_20250421125942.jsx
@@ -0,0 +1,12 @@
+// import albumData from '../data/song.json';
+// import SongCard from '../components/SongCard';
+
+// export const Playlist = () => {
+//   return (
+//     <main className="p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+//       {albumData.map((song, index) => (
+//         <SongCard key={index} {...song} />
+//       ))}
+//     </main>
+//   );
+// };
