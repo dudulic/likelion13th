2bea35a11ee9244214a22d3c03107bea30c78fa7
seungju <lsjxcs@gmail.com>
2025-05-14 00:21:54 +0900

    반응형 playlist

---
+++ week05/실습/.history/playlist/src/components&layout/Songcard_20250422120133.jsx
@@ -0,0 +1,13 @@
+const SongCard = ({ albumImage, releaseDate, artist, title, lyricist, composer }) => {
+    return (
+      <div className="bg-white rounded-xl shadow-md p-4">
+        <img src={albumImage} alt={`${title} 앨범 커버`} className="w-full rounded mb-4" />
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p className="text-gray-800">🎤 가수: {artist}</p>
+        <p className="text-gray-800">📝 작사가: {lyricist}</p>
+        <p className="text-gray-800">🎼 작곡가: {composer}</p>
+        <p className="text-gray-500 text-sm mt-2">📅 발매일: {releaseDate}</p>
+      </div>
+    );
+  };
+  
